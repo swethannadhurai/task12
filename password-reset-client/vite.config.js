e06f3bbf2782367913ce589827e5b39dcd31a75d
Swetha <swethaannadhurai@gmail.com>
2025-05-04 13:42:41 +0530

    Update Vite config for Netlify build

--- password-reset-client/vite.config.js
+++ password-reset-client/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public'
+  server: {
+    port: 5173,
+    strictPort: true, // Force Vite to use port 5173
+  },
 })
