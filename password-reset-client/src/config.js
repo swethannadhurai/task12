fb39a89dc2d39852ff62b881b4ceaf37d327ee31
Swetha <swethaannadhurai@gmail.com>
2025-05-05 13:46:32 +0530

    final updates

--- password-reset-client/src/config.js
+++ password-reset-client/src/config.js
@@ -3,6 +3,6 @@
 const API_BASE_URL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:4000'
-    : 'https://task12-4-f8f4.onrender.com';
+    : 'https://task12-1-nx76.onrender.com/';
 
 export default API_BASE_URL;
